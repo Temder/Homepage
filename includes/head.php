285122bc77a3b9f4d58b03956ffa6de443424c50
Temder <tom.ender03@googlemail.com>
2021-10-30 01:42:54 +0200

    aframe360

--- includes/head.php
+++ includes/head.php
@@ -8,3 +8,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.0.min.js"></script>
+<script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
--- includes/test.php
+++ includes/test.php
@@ -2,4 +2,11 @@
     <svg width="180" viewbox="0 0 90 90">
         <polygon points="20,45 45,20 70,45 62.5,45 62.5,70 50,70 50,55 45,55 45,43 47.5,43 50.5,40 50.5,35 47.5,32 42.5,32 39.5,35 39.5,40 42.5,43 45,43 45,55 40,55 40,70 27.5,70 27.5,45" />
     </svg>
+    
+    <a-scene>
+        <a-assets>
+            <img id="img360" src="background3.png" />
+        </a-assets>
+        <a-sky id="img-360" radius="10" src="#img360"> </a-sky>
+    </a-scene>
 </div>
