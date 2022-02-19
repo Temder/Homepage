0235857d06e1131b69bfb00f829a2df68ccd4793
Temder <tom.ender03@googlemail.com>
2022-02-19 13:38:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 
     <body>
         <?php
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+            
             // Main
             
             require_once('includes/db_connect.php'); // Database connection file
