f25491ebc345422172e29d2d5125b4c65c872890
Temder <tom.ender03@googlemail.com>
2023-01-05 20:15:08 +0100

    xxx

--- includes/settings.php
+++ includes/settings.php
@@ -46,3 +46,8 @@
     <datalist id="layouts">
     </datalist>
 </div>
+<div class="p-4">
+    <h3 class="lang en">Hidden Sections</h3>
+    <h3 class="lang de">Versteckte Bereiche</h3>
+    <input type="button" value="XXX" onclick="toggle()">
+</div>
